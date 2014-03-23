5ae5c1785584cdf6743a77587eb946fb0155b139
Neil <neil.ebrey@gmail.com>
2014-03-23 16:21:12 +0100

    First steps

---
+++ 00_FirstSteps.js
@@ -0,0 +1,9 @@
+//this is a comment
+
+var printThis = "Here is a string.";
+
+console.log(printThis);
+alert(printThis);
+
+var userInput = prompt("Please Enter your name...");
+console.log("Your name is " + userInput);
