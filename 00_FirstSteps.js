9c7eb66aabea612211014e9cdcf279280863d372
Neil <neil.ebrey@gmail.com>
2014-03-23 16:40:35 +0100

    More stuff

--- 00_FirstSteps.js
+++ 00_FirstSteps.js
@@ -1,9 +1,25 @@
 //this is a comment
+//not using semicolons because the GitHub style guide says not to!
 
-var printThis = "Here is a string.";
 
-console.log(printThis);
-alert(printThis);
+var printThis = "Here is a string."
 
-var userInput = prompt("Please Enter your name...");
-console.log("Your name is " + userInput);
+var testArray = [1,5,55,1000]
+
+console.log(printThis)
+alert(printThis)
+
+var userInput = prompt("Please Enter your name...")
+console.log("Your name is " + userInput)
+
+function Testfunction(input) {
+	return input * 100
+}
+
+console.log("\nAnd now a test function!...")
+console.log(Testfunction(10))
+
+console.log("\nAnd now something from an array!...")
+console.log(testArray[1])
+console.log("testArray[0] which is (" + testArray[0] + ") is of the type: " + typeof(testArray[0]))
+console.log("but printThis is obviously a " + typeof(printThis))
